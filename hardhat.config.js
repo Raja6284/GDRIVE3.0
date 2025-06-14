2150f4e292129faceddcc41ecee8ea62a417cf6a
Raja kumar <rajaraja360parth@gmail.com>
2025-06-14 18:35:24 +0530

    deployed the contract to sepolia testnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,11 +1,15 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.28",
   networks: {
     hardhat: {
       chainId: 31337,
+    },
+    sepolia: {
+      url: process.env.SEPOLIA_RPC_URL, // or use Alchemy
+      accounts: [process.env.PRIVATE_KEY], // Make sure this private key is from the 0.3 ETH account
     },
   },
   paths: {
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,7 +1,35 @@
+// const hre = require("hardhat");
+
+// async function main() {
+//   console.log("Starting deployment...");
+
+//   const Upload = await hre.ethers.getContractFactory("Upload");
+//   console.log("Contract factory obtained.");
+
+//   const upload = await Upload.deploy();
+//   console.log("Contract deployment transaction sent.");
+
+//   await upload.waitForDeployment();
+//   console.log("Upload contract deployed to:", upload.target);
+// }
+
+// main().catch((error) => {
+//   console.error("Failed to deploy:", error);
+//   process.exitCode = 1;
+// });
+
+
+
 const hre = require("hardhat");
 
 async function main() {
   console.log("Starting deployment...");
+
+  const [deployer] = await hre.ethers.getSigners();
+  console.log("Deploying from:", deployer.address);
+
+  const balance = await hre.ethers.provider.getBalance(deployer.address);
+  console.log("Deployer balance:", hre.ethers.formatEther(balance), "ETH");
 
   const Upload = await hre.ethers.getContractFactory("Upload");
   console.log("Contract factory obtained.");
